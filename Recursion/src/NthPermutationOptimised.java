9b17743f536e7fc365a45af02d8a881a7dfeb9a5
BKD471 <bhaskarkumardas9@gmail.com>
2022-12-26 01:40:57 +0530

    Day 11

---
+++ Recursion/src/NthPermutationOptimised.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution14 {
+    public static String getPermutation(int n, int k) {
+        int fact=1;
+        List<Integer> numbers=new ArrayList<>();
+        for(int i=1;i<n;i++){
+            fact*=i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+        String res="";
+        k=k-1;
+        while(true){
+            res=res+numbers.get(k/fact);
+            numbers.remove(k/fact);
+            if(numbers.size()==0) break;
+            k=k%fact;
+            fact=fact/numbers.size();
+        }
+        return res;
+    }
+}
+
+
+
+public class NthPermutationOptimised {
+    public static void main(String[] args) {
+        int n=4, k=15;
+        System.out.println(Solution14.getPermutation(n,k));
+    }
+}
