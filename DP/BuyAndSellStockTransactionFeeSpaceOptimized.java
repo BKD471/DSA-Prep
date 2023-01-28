31f380afe0f710aab8fdd5b8389b65ffc8d1dcd5
BKD471 <bhaskarkumardas9@gmail.com>
2023-01-28 23:55:15 +0530

    Day 41

--- DP/BuyAndSellStockTransactionFeeSpaceOptimized.java
+++ DP/BuyAndSellStockTransactionFeeSpaceOptimized.java
@@ -1,27 +1,26 @@
 public class BuyAndSellStockTransactionFeeSpaceOptimized {
-        public int maxProfit(int[] prices) {
-            int n=prices.length;
+    public int maxProfit(int[] prices, int fee) {
+        int n=prices.length;
 
-            int[] prev =new int[2];
-            int[] prevOfPrev=new int[2];
-            int[] curr=new int[2];
+        int prevBuy=0,prevNotBuy=0,currBuy=0,currNotBuy=0;
 
-            for(int index=n-1;index>=0;index--){
-                for(int canBuy=0;canBuy<=1;canBuy++){
-                    if(canBuy==1){
-                        int buy=-prices[index]+prev[0];
-                        int not_buy=prev[1];
-                        curr[canBuy]=Math.max(buy,not_buy);
-                    }else{
-                        int sell=prices[index]+prevOfPrev[1];
-                        int not_sell=prev[0];
-                        curr[canBuy]=Math.max(sell,not_sell);
-                    }
+
+        for(int index=n-1;index>=0;index--){
+            for(int canBuy=0;canBuy<=1;canBuy++){
+                if(canBuy==1){
+                    int buy=-prices[index]+prevNotBuy;
+                    int not_buy=prevBuy;
+                    currBuy=Math.max(buy,not_buy);
+                }else{
+                    int sell=prices[index]-fee+prevBuy;
+                    int not_sell=prevNotBuy;
+                    currNotBuy=Math.max(sell,not_sell);
                 }
-                prevOfPrev=(int[] )(prev.clone());
-                prev=(int[])(curr.clone());
             }
-
-            return curr[1];
+            prevBuy=currBuy;
+            prevNotBuy=currNotBuy;
         }
+
+        return  currBuy;
+    }
 }
