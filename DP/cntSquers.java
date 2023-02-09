33d1e2beabb23f272bf713d74ad574e9c6af3576
BKD471 <bhaskarkumardas9@gmail.com>
2023-02-10 00:10:10 +0530

    Day 52

---
+++ DP/cntSquers.java
@@ -0,0 +1,24 @@
+public class cntSquers {
+    public int countSquares(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0) dp[i][j]=0;
+                else if(i==0 || j==0)  dp[i][j]=1;
+                else dp[i][j]=1+Math.min(Math.min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]);
+            }
+        }
+
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cnt+=dp[i][j];
+            }
+        }
+
+        return cnt;
+    }
+}
