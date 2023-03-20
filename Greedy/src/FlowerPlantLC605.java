506bc679efd961de28b4732009174f085ec567a7
BKD471 <bhaskarkumardas9@gmail.com>
2023-03-21 00:29:04 +0530

    greedy

---
+++ Greedy/src/FlowerPlantLC605.java
@@ -0,0 +1,16 @@
+public class FlowerPlantLC605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int cnt=0;
+        for(int i=0;i<flowerbed.length;i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0)
+                    && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                flowerbed[i]=1;
+                cnt++;
+            }
+
+            if(cnt>=n) return true;
+        }
+
+        return false;
+    }
+}
