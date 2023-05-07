59dcd30791ea026efa9c6eef9323e9330b0246f9
BKD471 <bhaskarkumardas9@gmail.com>
2023-05-08 00:25:43 +0530

    d3

--- Graph/NumOfIslands.java
+++ Graph/NumOfIslands.java
@@ -1,28 +1,28 @@
 import java.util.Arrays;
 
 public class NumOfIslands {
-    public void lookUp(char[][] grid,boolean[][] visited,int row,int col,int m,int n){
+    public void dfs(char[][] grid,boolean[][] visited,int row,int col,int m,int n){
 
         visited[row][col]=true;
         //UP
         if(row>0 && !visited[row-1][col] && grid[row-1][col]=='1'){
-            lookUp(grid,visited,row-1,col,m,n);
+            dfs(grid,visited,row-1,col,m,n);
         }
 
         //DOWN
         if(row<m-1 && !visited[row+1][col] && grid[row+1][col]=='1'){
-            lookUp(grid,visited,row+1,col,m,n);
+            dfs(grid,visited,row+1,col,m,n);
         }
 
 
         //Left
         if(col>0 && !visited[row][col-1] && grid[row][col-1]=='1'){
-            lookUp(grid,visited,row,col-1,m,n);
+            dfs(grid,visited,row,col-1,m,n);
         }
 
         //Right
         if(col<n-1 && !visited[row][col+1] && grid[row][col+1]=='1'){
-            lookUp(grid,visited,row,col+1,m,n);
+            dfs(grid,visited,row,col+1,m,n);
         }
     }
 
@@ -31,7 +31,7 @@ public class NumOfIslands {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
-                    lookUp(grid,visited,i,j,m,n);
+                    dfs(grid,visited,i,j,m,n);
                     cnt++;
                 }
             }
