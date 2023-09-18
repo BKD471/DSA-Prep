344745685a3a74ee895e8a06c953b734bc382326
BKD471 <bhaskarkumardas9@gmail.com>
2023-09-19 01:05:10 +0530

    setup

---
+++ dynamicp/ROds.java
@@ -0,0 +1,28 @@
+package dynamicp;
+
+public class ROds {
+
+import java.util.*;
+
+    public class Solution {
+
+        private static int solve(int[] nums,int i,int n,int[][] memo){
+            if(i>=nums.length) return 0;
+            if(n<=0) return 0;
+
+            if(memo[i][n]!=-1) return memo[i][n];
+            int rodLength=i+1;
+            int take=Integer.MIN_VALUE;
+            if(rodLength<=n) take=nums[i]+solve(nums,i,n-rodLength,memo);
+            int not_take=solve(nums,i+1,n,memo);
+            memo[i][n]=Math.max(take,not_take);
+            return memo[i][n];
+        }
+        public static int cutRod(int price[], int n) {
+            // Write your code here.
+            int[][] memo=new int[n+1][n+1];
+            for(int i=0;i<=n;i++) Arrays.fill(memo[i],-1);
+            return solve(price, 0, n,memo);
+        }
+    }
+}
